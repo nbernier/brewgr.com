cf7c4fd7b5b2d027bbb7b679f142f445e2d28cb9
Nicolas Bernier <nicolas.bernier@cgi.com>
2017-03-31 14:07:56 -0400

    using newtonsoft to deserialize facebook token

--- Brewgr.Web.Core/Service/DefaultFacebookService.cs
+++ Brewgr.Web.Core/Service/DefaultFacebookService.cs
@@ -5,10 +5,18 @@ using System.Web;
 using Brewgr.Web.Core.Configuration;
 using Brewgr.Web.Core.Model;
 using Facebook;
+using Newtonsoft.Json;
 
 namespace Brewgr.Web.Core.Service
 {
-	public class DefaultFacebookService : IFacebookConnectService
+    public class FacebookToken
+    {
+        public virtual string access_token { get; set; }
+        public virtual string token_type { get; set; }
+        public virtual string bearer { get; set; }
+        public virtual string expires_in { get; set; }
+    }
+    public class DefaultFacebookService : IFacebookConnectService
 	{
 		private readonly IFacebookConnectSettings FacebookConnectSettings;
 
@@ -27,7 +35,7 @@ namespace Brewgr.Web.Core.Service
 		{
 			var accessToken = this.AcquireAccessTokenFromAuthCode(code, loginUrl);
 
-			var client = new FacebookClient(accessToken);
+			var client = new FacebookClient(accessToken.access_token);
 			dynamic result = client.Get("me", new { fields = "id, email, first_name, last_name" });
 
 			return new OAuthUserInfo
@@ -43,7 +51,7 @@ namespace Brewgr.Web.Core.Service
 		/// <summary>
 		/// Acquires an access token from an auth code
 		/// </summary>
-		string AcquireAccessTokenFromAuthCode(string code, string loginUrl)
+		FacebookToken AcquireAccessTokenFromAuthCode(string code, string loginUrl)
 		{
 			var url = string.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}",
 				this.FacebookConnectSettings.ApplicationKey,
@@ -55,9 +63,7 @@ namespace Brewgr.Web.Core.Service
 			var webClient = new WebClient();
 			var responseBody = webClient.DownloadString(url);
 
-			return responseBody.Split('&')
-				.First()
-				.Replace("access_token=", "");
+		    return JsonConvert.DeserializeObject<FacebookToken>(responseBody);
 		}
 	}
 }
